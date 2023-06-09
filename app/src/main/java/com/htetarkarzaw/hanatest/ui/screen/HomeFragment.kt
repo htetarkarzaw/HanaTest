75c5c18b36cc5d527d68f105fcb458c041265151
htetarkarzaw <arkarzaw412@gmail.com>
2023-06-09 15:43:31 +0630

    Initial commit

---
+++ app/src/main/java/com/htetarkarzaw/hanatest/ui/screen/HomeFragment.kt
@@ -0,0 +1,15 @@
+package com.htetarkarzaw.hanatest.ui.screen
+
+import com.htetarkarzaw.hanatest.databinding.FragmentHomeBinding
+import com.htetarkarzaw.hanatest.ui.base.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+    override fun observe() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initUi() {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/htetarkarzaw/hanatest/ui/screen/UserDetailFragment.kt
@@ -0,0 +1,18 @@
+package com.htetarkarzaw.hanatest.ui.screen
+
+import androidx.fragment.app.Fragment
+import com.htetarkarzaw.hanatest.databinding.FragmentUserDetailBinding
+import com.htetarkarzaw.hanatest.ui.base.BaseFragment
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class UserDetailFragment : BaseFragment<FragmentUserDetailBinding>(FragmentUserDetailBinding::inflate) {
+    override fun observe() {
+    }
+
+    override fun initUi() {
+    }
+
+
+}
